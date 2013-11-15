21fc1845e064c191f2a701ac8f797aac7cd9ed4f
Adam Stankiewicz <sheerun@sher.pl>
2013-11-16 00:18:24 +0100

    Make gem work for jruby 1.8.7 and simplify design

--- lib/filelock.rb
+++ lib/filelock.rb
@@ -1,25 +1,18 @@
 require 'filelock/version'
 require 'timeout'
+require 'tempfile'
 
 if RUBY_PLATFORM == "java"
   def Filelock(lockname, options = {}, &block)
+    lockname = lockname.path if lockname.is_a?(Tempfile)
     File.open(lockname, File::RDWR|File::CREAT, 0644) do |file|
       Thread.pass until file.flock(File::LOCK_EX)
       Timeout::timeout(options.fetch(:timeout, 60)) { yield }
     end
   end
-elsif RUBY_VERSION <= "1.8.7"
-  require 'tempfile'
-
-  def Filelock(lockname, options = {}, &block)
-    lockname = lockname.path if lockname.is_a?(Tempfile)
-    File.open(lockname, File::RDWR|File::CREAT, 0644) do |file|
-      file.flock(File::LOCK_EX)
-      Timeout::timeout(options.fetch(:timeout, 60)) { yield }
-    end
-  end
 else
   def Filelock(lockname, options = {}, &block)
+    lockname = lockname.path if lockname.is_a?(Tempfile)
     File.open(lockname, File::RDWR|File::CREAT, 0644) do |file|
       file.flock(File::LOCK_EX)
       Timeout::timeout(options.fetch(:timeout, 60)) { yield }

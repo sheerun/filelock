122e5e833e3a75562a316d6855026a9c5c29abb1
Kyle King <kylejking@gmail.com>
2016-03-04 23:44:08 -0700

    fixes issue 5

--- lib/filelock.rb
+++ lib/filelock.rb
@@ -9,7 +9,7 @@ if RUBY_PLATFORM == "java"
     lockname = lockname.path if lockname.is_a?(Tempfile)
     File.open(lockname, File::RDWR|File::CREAT, 0644) do |file|
       Thread.pass until Timeout::timeout(options.fetch(:wait, 60*60*24), Filelock::WaitTimeout) { file.flock(File::LOCK_EX) }
-      Timeout::timeout(options.fetch(:timeout, 60), Filelock::ExecTimeout) { yield }
+      Timeout::timeout(options.fetch(:timeout, 60), Filelock::ExecTimeout) { yield file }
     end
   end
 else
@@ -17,7 +17,7 @@ else
     lockname = lockname.path if lockname.is_a?(Tempfile)
     File.open(lockname, File::RDWR|File::CREAT, 0644) do |file|
       Timeout::timeout(options.fetch(:wait, 60*60*24), Filelock::WaitTimeout) { file.flock(File::LOCK_EX) }
-      Timeout::timeout(options.fetch(:timeout, 60), Filelock::ExecTimeout) { yield }
+      Timeout::timeout(options.fetch(:timeout, 60), Filelock::ExecTimeout) { yield file }
     end
   end
 end

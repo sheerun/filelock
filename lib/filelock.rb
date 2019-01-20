2466bed33fd45600a0e4c39a2dd214cf97508683
jaketothepast <windlejacob12@gmail.com>
2019-01-20 07:28:20 -0500

    Technically have overridden lock options and modes

--- lib/filelock.rb
+++ lib/filelock.rb
@@ -7,16 +7,18 @@ require 'tempfile'
 if RUBY_PLATFORM == "java"
   def Filelock(lockname, options = {}, &block)
     lockname = lockname.path if lockname.is_a?(Tempfile)
-    File.open(lockname, File::RDWR|File::CREAT, 0644) do |file|
-      Thread.pass until Timeout::timeout(options.fetch(:wait, 60*60*24), Filelock::WaitTimeout) { file.flock(File::LOCK_EX) }
+    modes = options.fetch(:modes, File::RDWR|File::CREAT)
+    lock_options = options.fetch(:lock_options, File::LOCK_EX)
+    File.open(lockname, modes, 0644) do |file|
+      Thread.pass until Timeout::timeout(options.fetch(:wait, 60*60*24), Filelock::WaitTimeout) { file.flock(lock_options) }
       Timeout::timeout(options.fetch(:timeout, 60), Filelock::ExecTimeout) { yield file }
     end
   end
 else
   def Filelock(lockname, options = {}, &block)
     lockname = lockname.path if lockname.is_a?(Tempfile)
-    File.open(lockname, File::RDWR|File::CREAT, 0644) do |file|
-      Timeout::timeout(options.fetch(:wait, 60*60*24), Filelock::WaitTimeout) { file.flock(File::LOCK_EX) }
+    File.open(lockname, modes, 0644) do |file|
+      Timeout::timeout(options.fetch(:wait, 60*60*24), Filelock::WaitTimeout) { file.flock(lock_options) }
       Timeout::timeout(options.fetch(:timeout, 60), Filelock::ExecTimeout) { yield file }
     end
   end
